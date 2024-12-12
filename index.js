1a621c1e758f38215bfc3b0085232f89a7328421
cack1e <124318005+cack1e@users.noreply.github.com>
2024-12-12 00:49:56 +0000

    text inputs & output converted to textareas.
    also fixed issues with cleanUpSlotter function, letting you input text with multiple line breaks in a row

--- index.js
+++ index.js
@@ -53,42 +53,6 @@ function replaceOther(transcript){
     return result;
 }
 
-function replaceOtherORIGINAL(transcript){ //CURRENT BUG: this function shits itself if the text you enter ends in a non-shorthand character
-    console.log("replaceOther START");
-    for(let x = 0; x<transcript.length;x++){
-        console.log("\nreplaceOther for loop start");
-        currentLetterShorthand = isThisShorthand(transcript[x]);
-        if(!(currentLetterShorthand)){
-            console.log("replaceOther if loop start");
-            lastOther = x; //index of last character thats not shorthand
-            console.log("lastOther = "+lastOther);
-            console.log("current letter = "+transcript[lastOther]);
-            while(!currentLetterShorthand && lastOther<transcript.length-1){ //while the current letter we have isnt in shorthand AND is not the last letter in the string
-                console.log("replaceOther while loop start");
-                currentLetterShorthand = isThisShorthand(transcript[lastOther+1]);//switching to next letter and checking if its shorthand
-                lastOther++; //increase last other bc if this character was shorthand, the while wouldve exited
-            }
-            //x is start index of non-shorthand character string, lastOther is the last index they appear
-            //we want to add a " in front of the first index and a "& after the last
-
-            //add quote in front of first index
-            transcript = transcript.substring(0, x) + "\"" + transcript.substring(x);
-
-            //now due to the extra quotation mark before the first index, the indices have increased by one, so we need to adjust our variables
-            lastOther++;
-
-            //add "& after last index
-            transcript = transcript.substring(0,lastOther) + "\"&" + transcript.substring(lastOther);
-
-            //we also need to advance x to skip past all the non-shorthand characters that have been surrounded by quotes
-            x=lastOther+1; //plus 1 because we want to skip the & after the last quote
-            console.log(transcript);
-        }
-    }
-    console.log("replaceOther END");
-    return transcript; //tested and working
-}
-
 function replaceShorthand(transcript){
     let shorthandToReplace;
     let replaceShorthandWith;
@@ -108,7 +72,10 @@ function replaceNewLine(transcript){
 
 function cleanUpSlotter(transcript){
     //if CHAR(10)& is surrounded by quotes then make it normal
-    transcript = transcript.replaceAll("\"CHAR(10)&\"&", "CHAR(10)&");
+    //transcript = transcript.replaceAll("\"CHAR(10)&\"&", "CHAR(10)&");
+
+    transcript = transcript.replaceAll("&\"CHAR(10)", "&CHAR(10)");
+    transcript = transcript.replaceAll("CHAR(10)&\"&", "CHAR(10)&");
 
     //if it ends in an '&' then chop it off
     if(transcript[transcript.length-1] == "&"){
